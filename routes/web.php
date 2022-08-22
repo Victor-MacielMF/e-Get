13af022d18f99c14a1ab2afce4c5c626f0f450b9
Victor-MacielMF <58239081+Victor-MacielMF@users.noreply.github.com>
2022-08-22 07:02:51 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,7 @@ use Illuminate\Support\Facades\Auth;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Auth::routes();
 
